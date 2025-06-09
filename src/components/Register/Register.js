94040b1ff2dfbca215cdff0f72e3b2efa76e4139
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2025-06-09 15:29:05 +0700

    setup Axios

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,26 @@
 import './Register.scss'
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { useHistory } from 'react-router-dom';
-
+import axios from 'axios';
+import { useEffect } from 'react';
 const Register = (props) => {
+
     let history = useHistory();
     const handleLogin = () => {
         history.push("/login");
+
+
     }
+
+    useEffect(() => {
+        axios.get("https://reqres.in/api/users/2").then(data => {
+            console.log("check data axios: ", data)
+        })
+
+    }, []);
+
+
+
     return (
         <div className="register-container d-flex align-items-center">
             <div className="container ">
