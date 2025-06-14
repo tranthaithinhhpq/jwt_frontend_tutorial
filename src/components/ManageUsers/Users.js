4e1204eed994952762d6f252a3fb1f4208b81dea
tranthaithinhhpq <tranthaithinhhpq@gmail.com>
2025-06-14 08:49:05 +0700

    video 53 display user

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,10 +1,77 @@
-import { useEffect } from "react";
-import './Users.scss'
+import { useEffect, useState } from "react";
+import './Users.scss';
+import { fetchAllUser } from "../../services/userService"
 const Users = (props) => {
+    const [listUsers, setListUsers] = useState([]);
+    useEffect(async () => {
+        fetchUsers();
+
+    }, [])
+    const fetchUsers = async () => {
+        let response = await fetchAllUser();
+        if (response && response.data && response.data.EC === 0) {
+            setListUsers(response.data.DT);
+            //  console.log(response.data.DT);
+        }
+    }
 
     return (
-        <div>
-            users components
+        <div className="container">
+            <div className="user-header">
+                <div className="title">
+                    <h3>Table Users</h3>
+                </div>
+                <div className="actions">
+                    <button className="btn btn-success">Refesh</button>
+                    <button className="btn btn-primary">Add new user</button>
+                </div>
+            </div>
+            <div className="user-body">
+                <table className="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">No</th>
+                            <th scope="col">Id</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Group</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listUsers && listUsers.length > 0 ?
+                            <>
+                                {listUsers.map((item, index) => {
+                                    return (
+                                        <tr key={`row-${index}`}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.id}</td>
+                                            <td>{item.email}</td>
+                                            <td>{item.username}</td>
+                                            <td>{item.Group ? item.Group.name : ""}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </>
+                            :
+                            <>
+                                <span>Not found users</span>
+                            </>}
+                    </tbody>
+                </table>
+            </div>
+            <div className="user-footer">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    </ul>
+
+                </nav>
+
+            </div>
         </div>
     )
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -11,5 +11,10 @@ const loginUser = (valueLogin, password) => {
     })
 }
 
+const fetchAllUser = () => {
+    return axios.get("http://localhost:8080/api/v1/user/read", {
+    })
+}
 
-export { registerNewUser, loginUser };
+
+export { registerNewUser, loginUser, fetchAllUser };
